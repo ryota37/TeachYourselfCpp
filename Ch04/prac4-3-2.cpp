3fad3bf060469f16e877bd743ab9c7c73f05bf7c
Ryota.Nakahama <ryota.nakahama@access-company.com>
2024-08-16 14:23:30 +0900

    start solving prac4-3-2

---
+++ Ch04/prac4-3-2.cpp
@@ -0,0 +1,18 @@
+// fix the program below
+// make the counter increase 0,1,2...
+
+#include <iostream>
+
+int get_counter()
+{
+    int counter = 0;
+    return counter++;
+}
+
+int main()
+{
+    std::cout << get_counter() << std::endl;
+    std::cout << get_counter() << std::endl;
+    std::cout << get_counter() << std::endl;
+    return 0;
+}
