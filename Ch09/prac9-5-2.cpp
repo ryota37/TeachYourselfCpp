c4923217f3d15ed74a7aa78c8f8429a8080d8e5b
Ryota.Nakahama <ryota.nakahama@access-company.com>
2025-01-06 17:34:15 +0900

    solved prac9-5

--- Ch09/prac9-5-2.cpp
+++ Ch09/prac9-5-2.cpp
@@ -1,1 +1,17 @@
 // Call a function template using type inference without specifying the template parameter.
+
+#include <iostream>
+
+template <typename T>
+void print(T a, T b)
+{
+    std::cout << a + b << std::endl;
+};
+
+int main()
+{
+    print(1, 2);
+    print(3.14, 2.71);
+
+    return 0;
+}
--- Ch09/prac9-5-3.cpp
+++ Ch09/prac9-5-3.cpp
@@ -1,2 +1,29 @@
 // Set a class template as a formal argument of a function.
 // Implement so that the template parameter of the class temlate is type infered via calling the function.
+
+#include <iostream>
+
+template <typename T>
+class A
+{
+private:
+    T a;
+    T b;
+public:
+    A(T a, T b) : a(a), b(b) {};
+    T add() { return a + b; };
+};
+
+template <typename T>
+void print(A<T> a)
+{
+    std::cout << a.add() << std::endl;
+};
+
+int main()
+{
+    print(A<int>(1, 2));
+    print(A<double>(3.14, 2.71));
+
+    return 0;
+}
