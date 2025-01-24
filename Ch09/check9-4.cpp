bafa67b6dc551706daa70f7b0f32729c48d45240
Ryota.Nakahama <ryota.nakahama@access-company.com>
2025-01-24 11:41:30 +0900

    solved check9-4, 9-5

--- Ch09/check9-4.cpp
+++ Ch09/check9-4.cpp
@@ -1,2 +1,27 @@
 // Define a function template so that the code cause a compile error
 // when the argument is a non-integer type.
+
+#include <iostream>
+
+template <typename T>
+class MyClass
+{
+    static_assert(std::is_integral<T>::value, "T must be an integral type.");
+public:
+    void print(T value)
+    {
+        std::cout << value << std::endl;
+    }
+};
+
+int main()
+{
+    MyClass<int> myClass;
+    myClass.print(123);
+
+    // Compile error
+    MyClass<double> myClass2;
+    myClass2.print(123.456);
+
+    return 0;
+}
